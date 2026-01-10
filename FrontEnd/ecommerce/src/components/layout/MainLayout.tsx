43d1e018905d86ffaa54fe0caa0e12cf1f1076f0
Eder Saldaña <eder.11.joel@gmail.com>
2026-01-10 12:54:08 -0500

    feat: se corrige centrado de las paginas.

--- FrontEnd/ecommerce/src/components/layout/MainLayout.tsx
+++ FrontEnd/ecommerce/src/components/layout/MainLayout.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "./Header";
+import "@/styles/layout.css";
 
 const MainLayout = () => {
   return (
     <div className="main-layout">
       <Header />
       <main className="content">
-        <Outlet /> {/* <--- ESTO ES VITAL: Aquí se renderizan Home, Cart, etc. */}
+        <Outlet />
       </main>
-      {/* <footer>Tu Footer aquí</footer> */}
     </div>
   );
 };
